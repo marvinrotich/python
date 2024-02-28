841b7871c4d70d1e798ad8cdddec532a6fd2bf5e
marvinrotich <160154304+marvinrotich@users.noreply.github.com>
2024-02-28 17:57:37 +0300

    Add files via upload

---
+++ # user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+
+name = input("What is your name? ")
+
+
+age = input("How old are you? ")
+
+
+location = input("Where do you live? ")
+
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
